13dec087d7c381a406cf7c775907f8b0532a5657
colson raphael <raphael.kasese@gmail.com>
2022-06-23 14:53:53 +0200

    mise à jour du menu hamburger

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,21 @@
 const btnMenuHamburger = document.querySelector('.block-menu-hamburger');
 const blockMenuItems  = document.querySelector('.block-menu');
+const navMenuItem = document.querySelectorAll('.nav-menu-item')
 
 btnMenuHamburger.addEventListener('click', () => {
-    blockMenuItems.classList.toggle('block-menu-visible');
-
+    //blockMenuItems.classList.toggle('block-menu-visible');
+    blockMenuItems.style.display = 'block';
 })
+
+for(let i = 0; i < navMenuItem.length; i++) {
+    navMenuItem[i].addEventListener('click', () => {
+        blockMenuItems.style.display = 'none';
+    })
+}
+
+
+
+
 
 // la modale menu items se ferme automatiquement
 Window.addEventListener('resize', () => {
