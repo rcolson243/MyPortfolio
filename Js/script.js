3d40b6a2fd59acb4a17cac5f5003a08eb1b4a120
colson raphael <raphael.kasese@gmail.com>
2022-06-24 09:35:58 +0200

    fixe un bug

--- Js/script.js
+++ Js/script.js
@@ -4,22 +4,17 @@ const navMenuItem = document.querySelectorAll('.nav-menu-item')
 
 btnMenuHamburger.addEventListener('click', () => {
     blockMenuItems.classList.toggle('block-menu-visible');
-    blockMenuItems.style.display = 'block';
 })
 
 for(let i = 0; i < navMenuItem.length; i++) {
     navMenuItem[i].addEventListener('click', () => {
-        blockMenuItems.style.display = 'none';
+        blockMenuItems.classList.toggle('block-menu-visible');
     })
 }
-
-
-
-
 
 // la modale menu items se ferme automatiquement
 Window.addEventListener('resize', () => {
     if (window.innerWidth > 600) {
-        blockMenuItems.classList.remove('block-menu-visible');
+        blockMenuItems.classList.toggle('block-menu-visible');
     };
 }) 
