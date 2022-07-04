1afe3e46f8da0bf73ea59c8c1480d38a616cc1ce
colson raphael <raphael.kasese@gmail.com>
2022-07-04 15:21:24 +0200

    ajout du buton au scroll et la nav top position = fixed

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 const btnMenuHamburger = document.querySelector('.block-menu-hamburger');
 const blockMenuItems  = document.querySelector('.block-menu');
-const navMenuItem = document.querySelectorAll('.nav-menu-item')
+const navMenuItem = document.querySelectorAll('.nav-menu-item');
 
 btnMenuHamburger.addEventListener('click', () => {
     blockMenuItems.classList.toggle('block-menu-visible');
@@ -8,13 +8,26 @@ btnMenuHamburger.addEventListener('click', () => {
 
 for(let i = 0; i < navMenuItem.length; i++) {
     navMenuItem[i].addEventListener('click', () => {
-        blockMenuItems.classList.toggle('block-menu-visible');
+        blockMenuItems.classList.remove('block-menu-visible');
     })
 }
 
 // la modale menu items se ferme automatiquement
-Window.addEventListener('resize', () => {
-    if (window.innerWidth > 600) {
-        blockMenuItems.classList.toggle('block-menu-visible');
-    };
-}) 
+// Window.addEventListener('resize', () => {
+//     if (window.innerWidth > 600) {
+//         blockMenuItems.classList.toggle('block-menu-visible');
+//     };
+// }) 
+
+const btnScrollbar = document.querySelector('.btn-scroll');
+
+window.addEventListener('scroll', (e) => {
+    console.log(window.scrollY);
+    // console.log(e);
+    if (scrollY > 315) {
+        btnScrollbar.style.display = "block";
+    }
+    else {
+        btnScrollbar.style.display = "none";
+    }
+})
